3beb8f0851fa1709c11356bd12c33abd278554bf
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-03-26 12:33:10 -0700

    Delete generateToken.js

--- generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken"; // run this command to use this library: npm install jsonwebtoken
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
-
-export default generateToken;
