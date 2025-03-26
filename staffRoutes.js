0988159eefe9e350d3a3b63eaf830cc22ec95de6
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-03-26 12:35:17 -0700

    Delete staffRoutes.js

--- staffRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerStaff } from "../controllers/staffController.js";
-
-const router = express.Router();
-
-router.post("/register", registerStaff);
-
-export default router;
