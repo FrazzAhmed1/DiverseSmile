3fd6d96af2cec3e16ecf52534515b1967315c9dd
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-03-26 12:33:56 -0700

    Delete patientRoutes.js

--- patientRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerPatient } from "../controllers/patientController.js";
-
-const router = express.Router();
-
-router.post("/register", registerPatient);
-
-export default router;
