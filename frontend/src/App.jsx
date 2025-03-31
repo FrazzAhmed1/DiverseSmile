bf05d6e1b016346fc84c5f6060d23e3280268784
babarchechi <bchechi530@gmail.com>
2025-03-30 18:44:43 -0700

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PatientSignup from "./pages/PatientSignup";
 import StaffSignup from "./pages/StaffSignup";
+import AdminSignup from "./pages/AdminSignup";
 import PatientLogin from "./pages/PatientLogin";
 import StaffLogin from "./pages/StaffLogin";
-import ForgotPassword from "./pages/ForgotPassword"; // Import ForgotPassword
+import AdminLogin from "./pages/AdminLogin";
+import PatientDashboard from "./pages/PatientDashboard";
+import StaffDashboard from "./pages/StaffDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import ForgotPassword from "./pages/ForgotPassword";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -14,9 +19,14 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/patient-signup" element={<PatientSignup />} />
         <Route path="/staff-signup" element={<StaffSignup />} />
+        <Route path="/admin-signup" element={<AdminSignup />} />
         <Route path="/patient-login" element={<PatientLogin />} />
         <Route path="/staff-login" element={<StaffLogin />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/staff-dashboard" element={<StaffDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/forgot-password" element={<ForgotPassword />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
