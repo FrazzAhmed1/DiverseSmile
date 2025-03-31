8ab18173e6f5154ad24767662207bb430f240127
Fraz Ahmed1 <ahmeddfraz887@gmail.com>
2025-03-30 19:56:44 -0700

    Delete backend/routes/patientLogRoutes1.js

--- backend/routes/patientLogRoutes1.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginPatient } from "../controllers/patientLogController.js";
-
-const router = express.Router();
-
-router.post("/login", loginPatient);
-
-export default router;
