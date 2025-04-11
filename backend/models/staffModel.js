9b631d906a1177f25772488073a1d271cc04afe1
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-04-11 16:30:47 -0700

    Jira 47 forgot password Update staffModel.js

--- backend/models/staffModel.js
+++ backend/models/staffModel.js
@@ -8,6 +8,8 @@ const staffSchema = mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "staff" },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
   },
   { timestamps: true }
 );
