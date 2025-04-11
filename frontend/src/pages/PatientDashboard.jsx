1a6ba71cbab625c8824bb26c46520f8ec7c5f24c
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-04-10 20:38:41 -0700

    PlaceHolder PatientDashboard

---
+++ frontend/src/pages/PatientDashboard.jsx
@@ -0,0 +1,8 @@
+// pages/PatientDashboard.jsx
+import Dashboard from "../components/Dashboard";
+
+const PatientDashboard = () => {
+    return <Dashboard role="patient" />;
+};
+
+export default PatientDashboard;
