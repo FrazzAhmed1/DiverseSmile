9321ae790b6b1ad13632d30e1d67dcf0cbbd25db
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-04-10 20:39:12 -0700

    PlaceHolder StaffDashboard

---
+++ frontend/src/pages/StaffDashboard.jsx
@@ -0,0 +1,8 @@
+// pages/StaffDashboard.jsx
+import Dashboard from "../components/Dashboard";
+
+const StaffDashboard = () => {
+    return <Dashboard role="staff" />;
+};
+
+export default StaffDashboard;
