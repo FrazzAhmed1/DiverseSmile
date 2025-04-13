3c7fc104ba20f77dac8f6ceafccd706bd552fcae
Mohamed Thameezudeen <60580568+iesa242@users.noreply.github.com>
2025-04-12 23:36:00 -0700

    Update ForgotPassword.jsx

--- frontend/DiverseSmile/src/pages/ForgotPassword.jsx
+++ frontend/DiverseSmile/src/pages/ForgotPassword.jsx
@@ -1,10 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import "../styles/ForgotPassword.css";
 
 const ForgotPassword = () => {
-    const navigate = useNavigate(); // Initialize useNavigate
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         firstName: "",
         lastName: "",
@@ -22,14 +21,12 @@ const ForgotPassword = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("Reset Password Request:", formData);
+        navigate("/otp-verification"); // Redirect to OTP verification page
     };
 
     return (
         <div className="forgot-password-page">
             <div className="forgot-password-container">
-                <Link to="/" className="go-back-button">
-                    Go Back
-                </Link>
                 <h2>Forgot Password</h2>
                 <form onSubmit={handleSubmit} className="forgot-password-form">
                     <div className="form-group">
@@ -62,7 +59,9 @@ const ForgotPassword = () => {
                             required
                         />
                     </div>
-                    <button type="submit" className="reset-password-button">Reset Password</button>
+                    <button type="submit" className="reset-password-button">
+                        Reset Password
+                    </button>
                 </form>
             </div>
         </div>
