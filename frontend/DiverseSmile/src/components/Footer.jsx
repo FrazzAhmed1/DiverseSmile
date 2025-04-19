67052ee813fac4ca7d63f6cc12db3baaa2ecd83b
Fraz Ahmed1 <ahmeddfraz887@gmail.com>
2025-04-19 01:25:43 -0700

    Fraz Ahmed JIRA Footer Feature
    A component added on every page with quick links and useful information for users.

---
+++ frontend/DiverseSmile/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+// Footer.jsx: footer rendering section of the website, including hours, etc.
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="main-footer">
+      <div className="footer-grid">
+        <div className="footer-section">
+          <h3>Contact Us</h3>
+          <p>New Patients: (916) 887-5455</p>
+          <p>Existing Patients: (916) 444-2222</p>
+          <p>6000 J Street, Sacramento, CA 95819</p>
+        </div>
+
+        <div className="footer-section">
+          <h3>Office Hours</h3>
+          <p>Monday - Friday: 9:00 AM-5:00 PM</p>
+        </div>
+
+        {/* Quick Links: Provides navigation links for patients, staff, and FAQs. */}
+        <div className="footer-section">
+          <h3>Quick Links</h3>
+          <div className="footer-links">
+            <Link to="/patient-login">Patient Account</Link>
+            <Link to="/staff-login">Staff Account</Link>
+            <Link to="/questions">FAQ</Link>
+          </div>
+        </div>
+      </div>
+
+      
+      <p className="copyright">© 2025 DiverseSmile. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
