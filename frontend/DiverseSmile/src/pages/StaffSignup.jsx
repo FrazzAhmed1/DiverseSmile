5b8d8f729db80a61a9a7534fed968328524dcc68
Suyog2004 <125492677+Suyog2004@users.noreply.github.com>
2025-04-25 22:09:47 -0700

    Update StaffSignup.jsx

--- frontend/DiverseSmile/src/pages/StaffSignup.jsx
+++ frontend/DiverseSmile/src/pages/StaffSignup.jsx
@@ -6,7 +6,7 @@ const StaffSignup = () => {
 
   const handleSubmit = async (formData) => {
     try {
-      const response = await fetch("http://localhost:3300/api/staff/register", {
+      const response = await fetch("http://localhost:5000/api/staff/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
