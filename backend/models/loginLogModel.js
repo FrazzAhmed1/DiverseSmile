9121a009b094a4ac105f27721c5e8e2aa88b79f6
Mohamed Thameezudeen <60580568+iesa242@users.noreply.github.com>
2025-04-25 23:03:30 -0700

    Update loginLogModel.js

--- backend/models/loginLogModel.js
+++ backend/models/loginLogModel.js
@@ -1,20 +1,29 @@
+// src/models/loginLogModel.js
+
 import mongoose from "mongoose";
 
-const loginLogSchema = new mongoose.Schema({
-  staffId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Staff",
-    required: true,
+const loginLogSchema = new mongoose.Schema(
+  {
+    staffId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Staff",
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    isManual: {
+      type: Boolean,
+      default: false,
+    },
   },
-  date: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: String,
-    required: true,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const LoginLog = mongoose.model("LoginLog", loginLogSchema);
 export default LoginLog;
