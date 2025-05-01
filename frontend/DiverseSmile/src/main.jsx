b3f328aab1a63a09e618998f200786a32139f890
babarchechi <bchechi530@gmail.com>
2025-04-30 17:36:22 -0700

    Update main.jsx

--- frontend/DiverseSmile/src/main.jsx
+++ frontend/DiverseSmile/src/main.jsx
@@ -1,13 +1,9 @@
-// main.jsx
-import React, { StrictMode, Suspense } from 'react'
-import { createRoot }      from 'react-dom/client'
-import App                  from './App.jsx'
-import './i18n'             // ← Make sure this import is here!
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Suspense fallback={<div>Loading translations…</div>}>
-      <App />
-    </Suspense>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
