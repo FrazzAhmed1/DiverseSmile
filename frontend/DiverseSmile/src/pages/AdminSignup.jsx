b5cdc10e4c5f13194c589a7e5fd9961ad7c4f669
Fraz Ahmed1 <ahmeddfraz887@gmail.com>
2025-04-30 19:19:25 -0700

    Update AdminSignup.jsx

--- frontend/DiverseSmile/src/pages/AdminSignup.jsx
+++ frontend/DiverseSmile/src/pages/AdminSignup.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import AdminSignupForm from "../components/AdminSignupForm";
 
 const AdminSignup = () => {
+    // store private key after registration 
     const [privateKey, setPrivateKey] = useState(null);
     const [message, setMessage] = useState("");
-
+    // handles admin registration form 
     const handleSignup = async (formData) => {
         try {
             const response = await fetch("http://localhost:5000/api/admin/register", {
@@ -16,12 +17,15 @@ const AdminSignup = () => {
 
             const data = await response.json();
             if (response.ok) {
+                // if successful, show private key and show success message 
                 setPrivateKey(data.privateKey);
                 setMessage("Admin registered! Save your private key above.");
             } else {
+                // show error message 
                 setMessage(data.message || "Registration failed.");
             }
         } catch (error) {
+            // for other failures or network issue
             setMessage("An error occurred.");
         }
     };
